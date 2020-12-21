fb0a9c116f7a569b0e0e5111b70bc44bb060a6f8
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2020-12-21 12:31:40 +0100

    Update style.js

--- style.js
+++ style.js
@@ -36,6 +36,7 @@
 				{ name: 'Lista ordenada 3', element: 'ol', attributes: { 'class': 'bck-ol bck-ol3' } },
 				{ name: 'Lista ordenada 4', element: 'ol', attributes: { 'class': 'bck-ol bck-ol4' } },
 				{ name: 'Lista ordenada 5', element: 'ol', attributes: { 'class': 'bck-ol bck-ol5' } },
+				{ name: 'Lista ordenada 6', element: 'ol', attributes: { 'class': 'bck-ol bck-ol6' } },
 
 
 				{ name: 'Lista desordenada 1', element: 'ul', attributes: { 'class': 'bck-ul bck-ul1' } },
